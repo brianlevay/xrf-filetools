eb0af0615a19c8a999696a9747f878a74e0b4b0a
Brian LeVay <brian_levay@yahoo.com>
2017-10-26 20:40:08 +0000

    writes unique name set to csv

--- src/uniqueNames/namesToCSV.go
+++ src/uniqueNames/namesToCSV.go
@@ -1,13 +1,33 @@
 package uniqueNames
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 // Highest level function //
 func (unique *UniqueNames) WriteToCSV(outPath string, outName string) error {
-	for key, value := range unique.Data {
-		fmt.Println("Key:", key, "Value:", value)
+	f, errC := os.Create(filepath.Join(outPath, outName))
+	if errC != nil {
+		log.Println(errC)
+		return errC
 	}
+	defer f.Close()
+
+	_, errW := f.WriteString("FolderName,FileName\n")
+	if errW != nil {
+		log.Println(errW)
+		return errW
+	}
+	for key, _ := range unique.Data {
+		_, errW = f.WriteString(key + "\n")
+		if errW != nil {
+			log.Println(errW)
+			return errW
+		}
+	}
+	f.Sync()
+
 	return nil
 }
