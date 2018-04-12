2e0abba810adedf83319f66fb3e3ab55451c9e69
Brian LeVay <brian_levay@yahoo.com>
2018-04-12 21:00:43 +0000

    added parseFolder to avaatechSpe

--- src/avaatechSpe/parseFolder.go
+++ src/avaatechSpe/parseFolder.go
@@ -1,8 +1,40 @@
 package avaatechSpe
 
-import ()
+import (
+	"os"
+	"strings"
+)
 
 func (spe *SPE) ParseFolder() error {
-	/// CONTINUE ///
+	var pathPts []string
+	var pathLength int
+	pathPts = strings.Split(spe.FilePath, string(os.PathSeparator))
+	pathLength = len(pathPts)
+	switch pathLength {
+	case 0:
+		{
+			spe.Folder = "Root"
+		}
+	case 1:
+		{
+			spe.Folder = "Root"
+		}
+	case 2:
+		{
+			if strings.Contains(pathPts[pathLength-2], "Run") {
+				spe.Folder = "Root"
+			} else {
+				spe.Folder = pathPts[pathLength-2]
+			}
+		}
+	default:
+		{
+			if strings.Contains(pathPts[pathLength-2], "Run") {
+				spe.Folder = pathPts[pathLength-3]
+			} else {
+				spe.Folder = pathPts[pathLength-2]
+			}
+		}
+	}
 	return nil
 }
